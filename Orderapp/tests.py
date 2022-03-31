6ed2accda1b33903f1bd7ce002ed003e16b5bcae
shahidctp <shahidpyn@gmail.com>
2022-03-31 15:28:53 +0530

    second

--- Orderapp/tests.py
+++ Orderapp/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
-
-# Create your tests here.
 '''
+# Create your tests here.
 echo "# athena" >> README.md
 git init
 git add README.md
@@ -9,6 +8,7 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/shahidctp/athena.git
 git push -u origin main
+
 
 
 '''
